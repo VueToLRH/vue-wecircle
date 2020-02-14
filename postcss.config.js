8e7eb421388a0a25ec5564fc17320da0ee264dfd
lrh <837233792@qq.com>
2020-02-14 11:00:19 +0800

    屏幕适配

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,13 @@ module.exports = {
   plugins: {
     autoprefixer: {},
     'postcss-px-to-viewport': {
-      viewportWidth: 375, // (Number) The width of the viewport.
-      viewportHeight: 667, // (Number) The height of the viewport.
-      unitPrecision: 3, // (Number) The decimal numbers to allow the REM units to grow to.
-      viewportUnit: 'vw', // (String) Expected units.
-      selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
-      minPixelValue: 1, // (Number) Set the minimum pixel value to replace.
-      mediaQuery: false // (Boolean) Allow px to be converted in media queries.
+      viewportWidth: 375, // 视窗的宽度，对应的是我们设计稿的宽度，Iphone6的一般是375 （xx/375*100vw）
+      viewportHeight: 667, // 视窗的高度，Iphone6的一般是667
+      unitPrecision: 3, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
+      viewportUnit: 'vw', // 指定需要转换成的视窗单位，建议使用vw
+      selectorBlackList: ['.ignore', '.hairlines'], // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
+      minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
+      mediaQuery: false // 允许在媒体查询中转换`px`
     }
   }
 }
